bafd00e9a1987c2a1a9a28e98420af26ccad8202
NiranjanDeshpande <niru.nj10@gmail.com>
2021-12-31 00:00:49 +0530

    Updated Multiple components app code

---
+++ multiple-components-app/src/Body.js
@@ -0,0 +1,8 @@
+import "./Body.css"
+function Body() {
+    return(
+        <p>Congratulation for subscribing Niranjan's Brand new Offers!</p>
+    )
+}
+
+export default Body;
---
+++ multiple-components-app/src/Header.js
@@ -0,0 +1,8 @@
+import "./Hedaer.css"
+function Header() {
+    return(
+        <h1>Welcome to Niranjan's world</h1>
+    )
+}
+
+export default Header;
